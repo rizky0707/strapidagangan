7b6cd012557639dca957c8b5efd854e739244051
riz007 <rezekim53@gmail.com>
2022-08-20 23:08:13 +0700

    make it better

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e91e01cb303ae8816d56d6b95bd6f150'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4afd5a54-1612-49be-afb0-91b2137b38dc'
+};
